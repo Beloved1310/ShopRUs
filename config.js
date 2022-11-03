5d8aacecc228120254e2d5c1b7fdf22d378f885e
Beloved1310 <adeoluwafisayomi@gmail.com>
2022-11-03 13:26:12 +0100

    refactor and added README

--- config.js
+++ config.js
@@ -4,11 +4,5 @@ dotenv.config();
 const { env } = process;
 
 module.exports = {
-//   JWT: env.JWT_KEY,
-//   CLOUDINARY_CLOUD_NAME: env.CLOUDINARY_CLOUD_NAME,
-//   CLOUDINARY_API_KEY: env.CLOUDINARY_API_KEY,
-//   CLOUDINARY_API_SECRET: env.CLOUDINARY_API_SECRET,
   PORT: env.PORT || 7000,
-//   MONGODBURI: env.MONGODBURI,
-//   debug: env.debug,
 };
